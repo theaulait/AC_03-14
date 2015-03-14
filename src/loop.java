a89229728043dde315076dff8fddee8b9cb89be0
theaulait <vanice.yee@gmail.com>
2015-03-14 11:32:31 -0400

    initial commit

---
+++ src/loop.java
@@ -0,0 +1,11 @@
+/**
+ * Created by c4q-vanice on 3/14/15.
+ */
+public class loop {
+    public static void main(String[] args){
+        int i=1;
+        for (i = 0; 1 < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
